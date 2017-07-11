0be205ab7ee742e341b6004ace65b2fc985e28b2
called-d <called.0xd@gmail.com>
2017-07-11 15:32:06 +0900

    summary of instance url

--- android/src/main/kotlin/jp/zero_x_d/workaholic/merltlreader/SettingsFragment.kt
+++ android/src/main/kotlin/jp/zero_x_d/workaholic/merltlreader/SettingsFragment.kt
@@ -3,7 +3,7 @@ package jp.zero_x_d.workaholic.merltlreader
 import android.os.Bundle
 import android.preference.EditTextPreference
 import android.preference.PreferenceFragment
-import android.util.Log
+
 
 /**
  * Created by quartz on 17/05/20.
@@ -14,10 +14,28 @@ class SettingsFragment: PreferenceFragment() {
 
         addPreferencesFromResource(R.xml.preferences)
 
-        val url_edittext = findPreference("pref_key_instance_url") as EditTextPreference
-        url_edittext.summary = url_edittext.text
-        // Log.i(getString(R.string.pref_title_instance_url), url_edittext.text)
+        fun validInstanceURL(urlString: String?): Boolean {
+            if (urlString.isNullOrBlank()) return false
+            // TODO まともに有効なインスタンスURLか判定する
+            return true
+        }
+        fun setSummary4IinstanceURL(
+                pref: EditTextPreference,
+                newValue: String?
+        ): Boolean {
+            val defaultSummary = getString(R.string.pref_summary_instance_url)
+            return if (validInstanceURL(newValue)) {
+                pref.summary = "$defaultSummary: $newValue"
+                true
+            } else {
+                false
+            }
+        }
+
+        val pref_instance_url = findPreference("pref_key_instance_url") as EditTextPreference
+        setSummary4IinstanceURL(pref_instance_url, pref_instance_url.text)
+        pref_instance_url.setOnPreferenceChangeListener { pref, any ->
+            setSummary4IinstanceURL(pref as EditTextPreference, any as String?)
+        }
     }
-
-
 }
